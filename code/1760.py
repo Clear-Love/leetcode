a614ba90f4975caeb2c8867c8206254f4d6f998c
lmio <2091319361@qq.com>
2023-09-23 10:18:27 +0800

    2023-9-23

--- code/1760.py
+++ code/1760.py
@@ -1,29 +1,20 @@
 '''
 Author: lmio 2091319361@qq.com
-Date: 2023-08-02 18:40:26
+Date: 2023-09-22 21:08:28
 LastEditors: lmio 2091319361@qq.com
-Description: 2760. 最长奇偶子数组
+Description: 1760. 袋子里最少数目的球
 '''
 
 from typing import List
 
 
 class Solution:
-    def longestAlternatingSubarray(self, nums: List[int], threshold: int) -> int:
-        n = len(nums)
-        dp = [0] * n
-        if nums[0] > threshold:
-            dp[0] = 0
-        elif nums[0] & 1:
-            dp[0] = 0
-        else:
-            dp[0] = 1
-        for i in range(1, n):
-            if nums[i] > threshold:
-                dp[i] = 0
-            elif dp[i-1] == 0 or (nums[i] & 1) ^ (nums[i-1] & 1) == 0:
-                dp[i] = 0 if nums[i] & 1 else 1
+    def minimumSize(self, nums: List[int], maxOperations: int) -> int:
+        left, right = 1, max(nums)
+        while left < right:
+            mid = (left+right) >> 1
+            if sum([(v-1)//mid for v in nums]) > maxOperations:
+                left = mid+1
             else:
-                dp[i] = dp[i-1] + 1
-        print(dp)
-        return max(dp)
+                right = mid
+        return left
